90521f8040835b3ef72d55aae2260286ab58be7a
Dhruv-Matariya <24bce304@nirmauni.ac.in>
2026-01-17 17:10:09 +0530

    debbuging axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+console.log("BACKEND URL =", import.meta.env.VITE_BACKEND_URL);
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
