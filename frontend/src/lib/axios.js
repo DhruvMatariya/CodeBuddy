196a7894f39dc6c041adba5f6915915bf65828e4
Dhruv-Matariya <24bce304@nirmauni.ac.in>
2026-01-17 17:22:46 +0530

    hardcoding axiosinstance

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 console.log("BACKEND URL =", import.meta.env.VITE_BACKEND_URL);
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: "https://codebuddy-xogz.onrender.com/api",
   withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
 });
 
